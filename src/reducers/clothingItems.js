0f07e80a7c925778ee7189a646ab25d92066708b
Peter Rosal <peter.rosal@gmail.com>
2016-11-08 21:58:00 -0500

    Add item toggle functionality

--- src/reducers/clothingItems.js
+++ src/reducers/clothingItems.js
@@ -1,9 +1,17 @@
 import data from '../data.json';
+import update from 'react-addons-update';
 
 const clothingItems = (state = data.items, action) => {
 	switch (action.type) {
 		case "TOGGLE_ITEM":
-			return state;
+			let itemIndex = state.findIndex((piece) => piece.id == action.item.id);
+			return update( state, {
+	      [itemIndex]: {
+          available: {
+            $set: !action.item.available
+          }
+        }
+      });
 		default:
 			return state;
 	}
