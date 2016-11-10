b8cfd2143556d97e7b39f21d946aa1649747995a
Peter Rosal <peter.rosal@gmail.com>
2016-11-09 23:09:30 -0500

    Add update clothing item and add 2 func to exp

--- src/reducers/clothingItems.js
+++ src/reducers/clothingItems.js
@@ -1,6 +1,8 @@
 import data from '../data.json';
 import update from 'react-addons-update';
 
+let itemIndex;
+
 const clothingItems = (state = data.items, action) => {
 	switch (action.type) {
 		case "TOGGLE_ITEM":
@@ -12,9 +14,19 @@ const clothingItems = (state = data.items, action) => {
           }
         }
       });
+    case 'UPDATE_CLOTHING_ITEM':
+    	itemIndex = getClothingItemIndex(state, action.item.id);
+    	return update(state, {
+    		[itemIndex]: {
+    			$set: action.item
+    		}
+    	});
 		default:
 			return state;
 	}
 }
 
 export default clothingItems;
+
+export const getClothingItem = (state, id) => state.find((item)=>item.id == id);
+export const getClothingItemIndex = (state, id) => state.findIndex((item)=>item.id == id);
