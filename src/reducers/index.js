3525b6914ecb219fab724ea86781c141957e4a78
Peter Rosal <peter.rosal@gmail.com>
2016-11-09 23:08:44 -0500

    Include addtional func to export and reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,20 @@
 import { combineReducers } from 'redux';
-import clothingItems from './clothingItems';
+import clothingItemDraft from './clothingItemDraft';
+import clothingItems, * as fromClothingItems from './clothingItems';
 import look from './look';
 import listItems from './listItems';
 
 const wearToApp = combineReducers({
+	clothingItemDraft,
 	clothingItems,
 	look,
 	listItems,
 })
 
 export default wearToApp;
+
+export const getClothingItem = (state, id) =>
+	fromClothingItems.getClothingItem(state.clothingItems, id);
+
+export const getClothingItemIndex = (state, id) =>
+	fromClothingItems.getClothingItemIndex(state.clothingItems, id);
