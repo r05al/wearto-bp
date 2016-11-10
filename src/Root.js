3e88e3759c49917cece0b583d67b400d7d94ffd6
Peter Rosal <peter.rosal@gmail.com>
2016-11-09 23:06:00 -0500

    Add clothingItem routes

--- src/Root.js
+++ src/Root.js
@@ -5,6 +5,10 @@ import { Provider } from 'react-redux';
 import reducer from './reducers';
 import App from './components/App';
 import css from '../public/styles.css';
+import { Router, Route, browserHistory } from 'react-router';
+import NewClothingItem from './components/NewClothingItem';
+import EditClothingItem from './components/EditClothingItem';
+
 
 const store = createStore(reducer);
 
@@ -13,7 +17,12 @@ export default class Root extends Component {
     return (
     	<div>
     		<Provider store={store}>
-    			<App />
+    			<Router history={browserHistory}>
+    				<Route path="/" component={App}>
+		    			<Route path="items/new" component={NewClothingItem} />
+		    			<Route path="items/:id/edit" component={EditClothingItem} />
+	    			</Route>
+	    		</Router>
     		</Provider>
     	</div>
     );
