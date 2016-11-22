f17cc404d4283b6c68798c65518889e99a615dd7
Peter Rosal <peter.rosal@gmail.com>
2016-11-21 19:08:14 -0500

    Add look reducer to add and update looks

--- src/reducers/looks.js
+++ src/reducers/looks.js
@@ -3,10 +3,25 @@ import update from 'react-addons-update';
 const looks = (state = [], action) => {
 	switch (action.type) {
 		case 'ADD_LOOK':
-		  return state;
+			let look = update(action.lookDraft, {
+				id: { $set: Date.now() }
+			});
+			return update(state, {
+				$push: [look]
+			});
+		case 'UPDATE_LOOK':
+			let lookIndex = getLookIndex(state, action.lookDraft.id);
+			return update(state, {
+				[lookIndex]: {
+					$set: action.lookDraft
+				}
+			});
 		default:
 			return state;
 	}
 }
 
 export default looks;
+
+export const getLook = (state, id) => state.find((look) => look.id == id);
+export const getLookIndex = (state, id) => state.findIndex((look) => look.id == id);
