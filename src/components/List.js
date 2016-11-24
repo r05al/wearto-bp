9f2f1d7aed97cbbf725ac5225bee953b59818506
Peter Rosal <peter.rosal@gmail.com>
2016-11-23 23:08:01 -0500

    Add immutable and refactor

--- src/components/List.js
+++ src/components/List.js
@@ -1,101 +1,111 @@
 import React, { Component, PropTypes } from 'react';
 import Slider from 'react-slick';
+import Immutable from 'immutable';
 import { Link } from 'react-router';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-class RightNavButton extends Component {
-  render() {
-			let style = {
-			left: 0,
-			position: "absolute",
-			zIndex: 1,
-			top: "30%",
-			opacity: ".7",
-			cursor: "pointer",
-			fontSize: "4em"
-		}
-    return <span{...this.props} style={style}>&#8249;</span>  
-  }
+const navButtonStyle = {
+	position: "absolute",
+	zIndex: 1,
+	top: "30%",
+	opacity: ".7",
+	cursor: "pointer",
+	fontSize: "4em"
 }
 
 class LeftNavButton extends Component {
   render() {
-		let style = {
-			right: 0,
-			position: "absolute",
-			zIndex: 1,
-			top: "30%",
-			opacity: ".7",
-			cursor: "pointer",
-			fontSize: "4em"
-		}
+		let style = Object.assign({}, navButtonStyle, { left: 0 });
+
+    return <span{...this.props} style={style}>&#8249;</span>  
+  }
+}
+
+class RightNavButton extends Component {
+  render() {
+  	let style = Object.assign({}, navButtonStyle, { right: 0 });
+
     return <span{...this.props} style={style}>&#8250;</span>  
   }
 }
 
 class List extends Component {
+	static propTypes = {
+		clothingItems: PropTypes.instanceOf(Immutable.List).isRequired,
+		listItems: PropTypes.instanceOf(Immutable.Map).isRequired,
+		selectItem: PropTypes.func.isRequired,
+		toggleItem: PropTypes.func.isRequired,
+		toggleList: PropTypes.func.isRequired,
+	};
 
+	shouldComponentUpdate(nextProps) {
+		return (!this.props.clothingItems.equals(nextProps.clothingItems) ||
+			!this.props.listItems.equals(nextProps.listItems));
+	}
 	
-	toggleList() {
+	handleToggle() {
 		this.props.toggleList(this.props.type);
 	}
 
 	render() {
 		let { type, listItems } = this.props;
-		let leftNav = <LeftNavButton />;
-		let rightNav = <RightNavButton />;
+		let nextNav = <RightNavButton />;
+		let prevNav = <LeftNavButton />;
 		let settings = {
 			infinite: true,
 			speed: 500,
 			slidesToShow: 7,
 			slidesToScroll: 1,
-			nextArrow: leftNav,
-			prevArrow: rightNav
+			nextArrow: nextNav,
+			prevArrow: prevNav
 		};
 
-		let clothingItems, slider;
-		if (listItems[type]) {
-			clothingItems = this.props.clothingItems
-			.filter((item) => item.type === type)
+		let slider;
+		if (listItems.get(type)) {
+			const clothingItems = this.props.clothingItems
+			.filter((item) => item.get('type') === type)
 			.map((item) => {
-  			let info;
-  			if (item.href.includes('placehold.it')) {
-  				info = <span onClick={ this.props.select.bind(null, item) }
-			  							 style={{position: "absolute", left: 0, textAlign: "center", width: "100%"}}>
-			  					{item.title}
+  			let info, href = item.get('href'),
+  				id = item.get('id');
+
+  			if (href.includes('placehold.it')) {
+  				info = <span onClick={ this.props.selectItem.bind(null, item) }
+			  							 style={{position: "absolute", left: 0, 
+			  							 				 textAlign: "center", width: "100%"}}>
+			  					{item.get('title')}
 			  				</span>;
   			}
+
 	  		return (
-	  			<div className='clothing' key={item.id}>
+	  			<div className='clothing' key={ id }>
 	  				<div style={{width: "90%", margin: "0 auto"}}>
-		  				<img src={ item.href }
-									 onClick={ this.props.select.bind(null, item) }
-									 style={ item.available ? {} : { filter: "opacity(50%)" }}/>
+		  				<img src={ href }
+									 onClick={ this.props.selectItem.bind(null, item) }
+									 style={ item.get('available') ? {} : { filter: "opacity(50%)" }}/>
 							{ info }
-		  				<div className="item-edit"><Link to={`items/${item.id}/edit`}>✎</Link></div>
+		  				<div className="item-edit"><Link to={`items/${id}/edit`}>✎</Link></div>
 		  				<div className="item-toggle" onClick={this.props.toggleItem.bind(null, item)}>&#9852;</div>
 
 		  			</div>
 	  			</div>
 	  		);
 	  	});
+
 		  slider = <Slider {...settings}>{clothingItems}</Slider>
 		}
 
-
     return (
     	<div className="list">
-	    	<div className={ listItems[type] ? 
+	    	<div className={ listItems.get(type) ? 
     					`list-title list-title--is-open ${type}` : 
     					`list-title ${type}`} 
-    					onClick={this.toggleList.bind(this)}>
+    					onClick={this.handleToggle.bind(this)}>
 	      </div>
     		<ReactCSSTransitionGroup transitionName="toggle"
     		                         transitionEnterTimeout={500}
     		                     		 transitionLeaveTimeout={500}>
 		  	{ slider }
 		  	</ReactCSSTransitionGroup>
-
 	  	</div>
     );
 	}
