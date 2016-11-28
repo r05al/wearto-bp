429c6653e4eeef310b92193fc43d79dd55243b79
Peter Rosal <peter.rosal@gmail.com>
2016-11-28 13:57:24 -0500

    Create store at index and clean up routes

--- src/Root.js
+++ src/Root.js
@@ -1,34 +1,27 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { createStore } from 'redux';
+import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
-import reducer from './reducers';
-import App from './components/App';
-import css from '../public/styles.css';
 import { Router, Route, browserHistory } from 'react-router';
-import NewClothingItem from './components/NewClothingItem';
-import EditClothingItem from './components/EditClothingItem';
-import NewLook from './components/NewLook';
-import EditLook from './components/EditLook';
+import App from './containers/App';
+import NewClothingItem from './containers/NewClothingItem';
+import EditClothingItem from './containers/EditClothingItem';
+import NewLook from './containers/NewLook';
+import EditLook from './containers/EditLook';
 
+const Root = ({ store }) => (
+	<Provider store={store}>
+		<Router history={browserHistory}>
+			<Route path="/" component={App}>
+        <Route path="items/new" component={NewClothingItem} />
+        <Route path="items/:id/edit" component={EditClothingItem} />
+  			<Route path="looks/new" component={NewLook} />
+  			<Route path="looks/:id/edit" component={EditLook} />
+			</Route>
+		</Router>
+	</Provider>
+);
 
-const store = createStore(reducer);
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+};
 
-export default class Root extends Component {
-  render() {
-    return (
-    	<div>
-    		<Provider store={store}>
-    			<Router history={browserHistory}>
-    				<Route path="/" component={App}>
-              <Route path="items/new" component={NewClothingItem} />
-              <Route path="items/:id/edit" component={EditClothingItem} />
-		    			<Route path="looks/new" component={NewLook} />
-		    			<Route path="looks/:id/edit" component={EditLook} />
-	    			</Route>
-	    		</Router>
-    		</Provider>
-    	</div>
-    );
-  }
-}
+export default Root;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Root from './Root';
+import { createStore } from 'redux';
+import reducer from './reducers';
+import css from '../public/styles.css';
 
-render(<Root />, document.getElementById('root'));
+const store = createStore(reducer);
+
+render(<Root store={store}/>, document.getElementById('root'));
